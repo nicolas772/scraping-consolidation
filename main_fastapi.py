f57cc0e408a82aeb1ac0e1327634894738f77eeb
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-05-30 14:36:00 -0400

    send message from fastapi

--- main_fastapi.py
+++ main_fastapi.py
@@ -1,15 +1,53 @@
 from typing import Union
 
 from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel
+import boto3
 
 app = FastAPI()
+
+# Configurar el cliente SQS
+sqs_client = boto3.client('sqs', region_name='us-east-1')  # Especifica tu región
+queue_url = 'https://sqs.us-east-1.amazonaws.com/533267246902/EC2-to-Lambda'
 
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
+@app.put("/send-message")
+def send_message():
+    """
+    bucket_name = request.json.get('bucket_name')
+    root_file = request.json.get('root_file')
+    num_files = request.json.get('num_files')
+    """
+    bucket_name = 'bank-consolidation-2'
+    root_file = 'cartola_parte_'
+    num_files = 1 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    # Mensaje que deseas enviar
+    message_body = 'New Excel File from EC2'
+
+    # Atributos del mensaje
+    message_attributes = {
+        'bucket-name': {
+            'StringValue': bucket_name,
+            'DataType': 'String'
+        },
+        'root-file': {
+            'StringValue': root_file,
+            'DataType': 'String'
+        },
+        'num-files': {
+            'StringValue': num_files,
+            'DataType': 'String'
+        }
+    }
+    # Enviar el mensaje
+    response = sqs_client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=message_body,
+        MessageAttributes=message_attributes
+    )
+    return {'message': 'Mensaje enviado correctamente a la cola SQS'}
